b700aa6284d562c6f669def92797abe85204854e
annh.dev <annh.dev@gmail.com>
2025-04-26 00:26:25 +0700

    Add Vietnamese

--- TTS/tts/layers/xtts/tokenizer.py
+++ TTS/tts/layers/xtts/tokenizer.py
@@ -628,7 +628,7 @@ class VoiceBpeTokenizer:
             )
 
     def preprocess_text(self, txt, lang):
-        if lang in {"ar", "cs", "de", "en", "es", "fr", "hu", "it", "nl", "pl", "pt", "ru", "tr", "zh", "ko"}:
+        if lang in {"ar", "cs", "de", "en", "es", "fr", "hu", "it", "nl", "pl", "pt", "ru", "tr", "zh", "ko", "vi"}:
             txt = multilingual_cleaners(txt, lang)
             if lang == "zh":
                 txt = chinese_transliterate(txt)
